e2dc6beaa6300a664b829b22d0ca5b8e402e9f9e
Tomasz Cudziło <tomasz@luzid.co>
2011-10-25 10:34:22 +0200

    add variable length of the arrays to be sorted

--- SorterTest.java
+++ SorterTest.java
@@ -2,35 +2,34 @@ import java.util.Arrays;
 
 public class SorterTest {
 
-  private static final int COUNT = 10000;
+  private final static int MAX_POWER = 4;
 
   public static void main(String[] args) {
+    double[] array = ArrayHelper.generate((int) Math.pow(10, SorterTest.MAX_POWER));
 
-    double[] array = ArrayHelper.generate(SorterTest.COUNT);
-    double[] array_for_qsort = Arrays.copyOf(array, array.length);
-    double[] array_for_insort = Arrays.copyOf(array, array.length);
+    for (int i = 1; i <= SorterTest.MAX_POWER; i++) {
+      int length = (int) Math.pow(10, i);
 
-    long[] estimates = new long[2];
-    double[] miliseconds = new double[2];
-    
-    // System.out.println("before sort:");
-    // ArrayHelper.print(array);
-    
-    estimates[0] = Sorter.qsort(array_for_qsort);
-    // System.out.println("after qsort():");
-    // ArrayHelper.print(array_for_qsort);
-    
-    estimates[1] = Sorter.insort(array_for_insort);
-    // System.out.println("after insort():");
-    // ArrayHelper.print(array_for_insort);
+      double[] array_for_qsort = Arrays.copyOf(array, length);
+      double[] array_for_insort = Arrays.copyOf(array, length);
 
-    for(int i = 0; i < estimates.length; i++) {
-      miliseconds[i] = estimates[i] / Math.pow(10, 6);
+      long[] estimates = new long[2];
+      
+      // System.out.println("before sort:");
+      // ArrayHelper.print(array);
+      
+      estimates[0] = Sorter.qsort(array_for_qsort);
+      // System.out.println("after qsort():");
+      // ArrayHelper.print(array_for_qsort);
+      
+      estimates[1] = Sorter.insort(array_for_insort);
+      // System.out.println("after insort():");
+      // ArrayHelper.print(array_for_insort);
+
+      System.out.format("sorting %d doubles took:%n", length);
+      System.out.format("* qsort:  %16d [ns]%n", estimates[0]);
+      System.out.format("* insort: %16d [ns]%n", estimates[1]);
     }
-
-    System.out.format("sorting %d doubles took:%n", SorterTest.COUNT);
-    System.out.format("* qsort:  %10.3f [ms]%n", miliseconds[0]);
-    System.out.format("* insort: %10.3f [ms]%n", miliseconds[1]);
   }
 
 }
